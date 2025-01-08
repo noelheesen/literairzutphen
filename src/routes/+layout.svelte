01ba94e9f02ea964df516148ebc2688c7b372c9c
Noel Heesen <noelheesen@gmail.com>
2025-01-08 05:06:18 +0100

    add basic page setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script lang=ts>
+	import '../app.css';
+	import Links from '../../data/links.json'
+	import { ALPHABET } from '$lib/constants';
+
+	let { children } = $props();
+</script>
+
+<header class="h-12 sticky border-b-4 border-red-800">
+	<h1>Literair Zutphen</h1>
+	<menu>
+		<ul class="flex float-right">
+			{#each ALPHABET as letter}
+				<li>{letter}</li>
+			{/each}
+		</ul>
+	</menu>
+</header>
+
+<aside class="border-r-2 inline-block">
+	<menu>
+		<li><a href="/">Home</a></li>
+		<li><a href="/introductie">Introductie</a></li>
+		<h4>Tijdvak</h4>
+		<ul>
+			<li><a href="/tijdvak/voor-1500">voor 1500</a></li>
+			<li><a href="/tijdvak/1500-1700">1500 - 1700</a></li>
+			<li><a href="/tijdvak/1700-1800">1700 - 1800</a></li>
+			<li><a href="/tijdvak/1800-1900">1800 - 1900</a></li>
+			<li><a href="/tijdvak/1900-1945">1900 - 1945</a></li>
+			<li><a href="/tijdvak/1945-heden">1945 - heden</a></li>
+		</ul>
+		<li><a href="/citaten-en-gedichten">Citaten, gedichten en teksten</a></li>
+		<li><a href="/colofon">Colofon / Contact</a></li>
+	</menu>
+
+	<h4>Links</h4>
+	<ul>
+		{#each Links as link}
+			<li><a href={link.url} target="_blank" rel="noopener noreferrer" title={link.title}>{link.text}</a></li>
+		{/each}
+	</ul>
+</aside>
+
+<main>{@render children()}</main>
---
+++ src/routes/colofon/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Colofon / Contact</h1>
+
+<p>
+    Literair Zutphen is het resultaat van twintig jaar liefdewerk, enkele eeuwen oud papier en een paar jaar nieuwe media. De bijeenvergaarde informatie is afkomstig uit archieven en bibliotheken, waarvan er drie met name moeten worden bedankt, te weten de Koninklijke Bibliotheek en het Literatuurmuseum in Den Haag en het Regionaal Archief Zutphen.
+    <br />
+    <br />
+    Om de teksten te verlevendigen en de belangstelling voor de beschreven auteurs te vergroten zijn er waar mogelijk portretfoto’s toegevoegd. Indien u, hetzij als geportretteerde hetzij als fotograaf, bezwaar heeft tegen plaatsing van een foto, hoor ik het graag: dan zal ik de foto verwijderen en proberen te vervangen door een andere.
+    <br />
+    <br />
+    Deze site is work in progress en zal dus regelmatig worden bijgewerkt en uitgebreid. Ik heb niet de illusie volledig te zijn. Aanvullingen, verbeteringen en tips zijn van harte welkom. U kunt deze sturen naar hans.heesen@gmail.com.
+</p>
